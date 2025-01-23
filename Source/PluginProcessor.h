dab7b7d2c619e50fd793328a02a87437f88b94e3
Dan Dubois <74828217+schematicdnb@users.noreply.github.com>
2025-01-23 12:19:54 -0500

    Colour tracking consistent across entire scroll
    Bug: dark segments at beginning of playback from empty buffer

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -21,10 +21,10 @@ namespace juce
     public:
         RGBMeter() : AudioVisualiserComponent(1)
         {
-//            while (colHistory.size() < 512)
-//            {
-//                colHistory.add(Colour(0, 0, 0));
-//            }
+            while (colHistory.size() < 512)
+            {
+                colHistory.add(Colour(0, 0, 0));
+            }
 
             setBufferSize(512);
             setSamplesPerBlock(256);
@@ -32,34 +32,29 @@ namespace juce
             setRepaintRate(30);
         }
 
+        // int r = 255, g = 0, b = 0;
+        int r, g, b;
         Array<Colour> colHistory;
-
-        // void colourPath(Path &path, const Range<float> *levels,
-        //                 int numLevels, int nextSample)
-        // {
-        //     path.preallocateSpace(4 * numLevels + 8);
-
-        //     for (int i = 0; i < numLevels; ++i)
-        //     {
-        //         auto level = -(levels[(nextSample + i) % numLevels].getEnd());
-
-        //         if (i == 0)
-        //             path.startNewSubPath(0.0f, level);
-        //         else
-        //             path.lineTo((float)i, level);
-
-        //     }
-
-        //     for (int i = numLevels; --i >= 0;)
-        //         path.lineTo((float)i, -(levels[(nextSample + i) % numLevels].getStart()));
-
-        //     path.closeSubPath();
-        // }
 
         void paintChannel(Graphics &graphic, Rectangle<float> area,
                           const Range<float> *levels, int numLevels, int nextSample) override
         {
-            int r, g, b;
+
+            // if (b == 255)
+            // {
+            //     r = 255;
+            //     g = 0;
+            //     b = 0;
+            // }
+            // if (g < 255)
+            // {
+            //     g++;
+            // }
+            // //            if (r == 255) {g++;}
+            // if (g == 255)
+            // {
+            //     b++;
+            // }
             r = 255;
             g = Random::getSystemRandom().nextInt(256);
             b = Random::getSystemRandom().nextInt(256);
@@ -67,30 +62,7 @@ namespace juce
             //            b = 255;
 
             Colour c = Colour(r, g, b);
-            if (colHistory.size() > numLevels) {
-                colHistory.remove((nextSample + 1) % numLevels);
-            }
-            colHistory.add(c);
-            
-//            std::cout << numLevels << std::endl;
-
-            // Path path;
-            // path.preallocateSpace(4 * numLevels + 8);
-
-            // for (int i = 0; i < numLevels; ++i)
-            // {
-            //     auto level = -(levels[(nextSample + i) % numLevels].getEnd());
-
-            //     if (i == 0)
-            //         path.startNewSubPath(0.0f, level);
-            //     else
-            //         path.lineTo((float)i, level);
-            // }
-
-            // for (int i = numLevels; --i >= 0;)
-            //     path.lineTo((float)i, -(levels[(nextSample + i) % numLevels].getStart()));
-
-            // path.closeSubPath();
+            colHistory.set(nextSample, c);
 
             for (int i = 0; i < numLevels; ++i)
             {
@@ -103,36 +75,11 @@ namespace juce
                 path.startNewSubPath((float)i, start);
                 path.lineTo((float)i, end);
                 path.closeSubPath();
-                
+
                 // colour the segment
                 graphic.setColour(colHistory[(nextSample + i) % numLevels]);
                 graphic.strokePath(path, PathStrokeType(1.0f), AffineTransform::fromTargetPoints(0.0f, -1.0f, area.getX(), area.getY(), 0.0f, 1.0f, area.getX(), area.getBottom(), (float)numLevels, -1.0f, area.getRight(), area.getY()));
             }
-
-            // for (int i = numLevels; --i >= 0;)
-            //     path.lineTo((float)i, -(levels[(nextSample + i) % numLevels].getStart()));
-
-            // path.closeSubPath();
-
-            // std::cout << nextSample << std::endl;
-
-            // Path p;
-            // colourPath(p, segments, numSegments, nextSample);
-
-            //            for (int i = 0; i < numSegments; i++)
-            //            {
-            //                Path segment;
-            //                segment.startNewSubPath((float)i, -(segments[(nextSample + i) % numSegments].getEnd()));
-            //                segment.lineTo((float)i, -(segments[(nextSample + i) % numSegments].getStart()));
-            //                segment.closeSubPath();
-            //
-            //                graphic.setColour(colHistory[i]);
-            //                graphic.strokePath(segment, PathStrokeType(1.0f), AffineTransform::fromTargetPoints(0.0f, -1.0f, area.getX(), area.getY(), 0.0f, 1.0f, area.getX(), area.getBottom(), (float)numSegments, -1.0f, area.getRight(), area.getY()));
-            //            }
-
-            // graphic.fillPath(p, AffineTransform::fromTargetPoints(0.0f, -1.0f, area.getX(), area.getY(),
-            //                                                 0.0f, 1.0f, area.getX(), area.getBottom(),
-            //                                                 (float)numSegments, -1.0f, area.getRight(), area.getY()));
         }
     };
 }

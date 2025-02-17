e943201959f5160d0cde14652928026ddf26eadf
Dan Dubois <74828217+schematicdnb@users.noreply.github.com>
2025-02-17 17:26:38 -0500

    Waveform stability fixed

--- Source/RGBMeter.cpp
+++ Source/RGBMeter.cpp
@@ -14,103 +14,135 @@ namespace juce
     RGBMeter::RGBMeter()
     {
 
-        slidingWindow.setSize(1, slidingWindowSize);
-
         // initialize with full array of default values
         for (int i = 0; i < displayLength * sampleRate; i++)
         {
             waveformSamples.add(std::make_tuple(0.0f, Colour(Colours::black)));
         }
 
-        // auto writePointer = waveformSamples.getWritePointer();
-
         startTimerHz(60);
+        //        startTimerHz(60);
     }
-
-    //    void RGBMeter::prepare(dsp::ProcessSpec spec)
-    //    {
-    //        waveformSamples.clear();
-    //        waveformSamples.insertMultiple(0, std::make_tuple(0.0f, Colour(Colours::black)), displayLength * spec.sampleRate);
-    //    }
 
     void RGBMeter::pushSamples(const AudioBuffer<float> &buffer)
     {
         // skip sliding window and fft for now
 
-        //        Colour colour = Colour(Colours::white);
-        Colour colour = Colour::fromRGB(Random::getSystemRandom().nextInt(256),
-                                        Random::getSystemRandom().nextInt(256),
-                                        Random::getSystemRandom().nextInt(256));
+//        Colour colour = Colour(Colours::white);
+                Colour colour = Colour::fromRGB(Random::getSystemRandom().nextInt(256),
+                                                Random::getSystemRandom().nextInt(256),
+                                                Random::getSystemRandom().nextInt(256));
 
-        skip = std::floor(waveformSamples.size() / this->getWidth()); // temporary
+        width = this->getWidth();
+        if (!width)
+        {
+            return;
+        } // do nothing, no point in processing buffer if can't be displayed
+
+        // initialize/reset the display buffer if it does not match the display width
+        if (displayBuffer.size() != width)
+        {
+            displayBuffer = CircularBuffer<std::tuple<Range<float>, Colour>>(width);
+            for (int i = 0; i < width; i++)
+            {
+                displayBuffer.add(std::make_tuple(Range<float>(0.0f, 0.0f), Colour(Colours::white)));
+            }
+            chunkSize = std::floor(bufferLength / width); // temp hardcode
+            counter = 0;
+            chunkBuffer = AudioBuffer<float>(1, chunkSize);
+        }
 
         for (int i = 0; i < buffer.getNumSamples(); i++)
         {
-            auto sample = buffer.getSample(1, 0);
+            auto sample = buffer.getSample(0, 0);
             waveformSamples.add(std::make_tuple(sample, colour));
-            if (offset == 0)
+            chunkBuffer.setSample(0, counter, sample);
+
+            if (++counter == chunkSize)
             {
-                offset = skip - 1;
-            }
-            else
-            {
-                offset--;
+                auto level = chunkBuffer.findMinMax(0, 0, chunkSize);
+
+                displayBuffer.add(std::make_tuple(level, colour));
+                counter = 0;
             }
         }
     }
 
     void RGBMeter::paint(Graphics &g)
     {
-        //        DBG("drawing samples");
-        //        auto skip = std::floor(waveformSamples.size() / this->getWidth());
-
-        int x = 0;
-        for (int sample = offset; sample < waveformSamples.size() - skip; sample += skip)
+        if (!width)
         {
+            return;
+        } // nothing to display
 
-            auto min = std::numeric_limits<float>::max();
-            auto max = std::numeric_limits<float>::lowest();
-            for (int i = sample; i <= sample + skip; ++i)
-            {
-                auto level = std::get<0>(waveformSamples.get(i));
-                if (level < min)
-                {
-                    min = level;
-                }
-                if (level > max)
-                {
-                    max = level;
-                }
-            }
+        for (int i = 0; i < displayBuffer.size(); i++)
+        {
+            auto tuple = displayBuffer.get(i);
+            auto range = std::get<0>(tuple);
+            auto colour = std::get<1>(tuple);
+            auto min = range.getStart();
+            auto max = range.getEnd();
 
-            auto colour1 = std::get<1>(waveformSamples.get(sample));
-
-            // auto level2 = std::get<0>(waveformSamples.get(sample + skip));
-            auto colour2 = std::get<1>(waveformSamples.get(sample + skip));
-
-            auto y1 = jmap(min, -1.0f, 1.0f, (float)this->getHeight(), (float)0);
-            auto y2 = jmap(max, -1.0f, 1.0f, (float)this->getHeight(), (float)0);
+            auto y1 = jmap(min, -1.0f, 1.0f, (float)this->getHeight(), 0.0f);
+            auto y2 = jmap(max, -1.0f, 1.0f, (float)this->getHeight(), 0.0f);
 
             Path p;
-            p.startNewSubPath(x, y1);
-            p.lineTo(x, y2);
+            p.startNewSubPath(i, y1);
+            p.lineTo(i, y2);
             p.closeSubPath();
 
-            // TODO: colour gradient
-//            //             Create a gradient from colour1 to colour2
-//            ColourGradient gradient(colour1, x, y1,
-//                                    colour2, x+1, y2, false);
-////            g.setGradientFill(gradient);
-////            g.fillPath(p);
-//            GradientPathStrokeType gradientStroke(1.0f, gradient);
-//            g.strokePath(p, gradientStroke);
-
-            g.setColour(colour1);
+            g.setColour(colour);
             g.strokePath(p, PathStrokeType(1.0f));
-
-            
-            x++;
         }
+
+        //        auto skip = std::floor(waveformSamples.size() / this->getWidth());
+
+        //        int x = 0;
+        //        for (int sample = offset; sample < waveformSamples.size() - skip; sample += skip)
+        //        {
+        //
+        //            auto min = std::numeric_limits<float>::max();
+        //            auto max = std::numeric_limits<float>::lowest();
+        //            for (int i = sample; i <= sample + skip; ++i)
+        //            {
+        //                auto level = std::get<0>(waveformSamples.get(i));
+        //                if (level < min)
+        //                {
+        //                    min = level;
+        //                }
+        //                if (level > max)
+        //                {
+        //                    max = level;
+        //                }
+        //            }
+        //
+        //            auto colour1 = std::get<1>(waveformSamples.get(sample));
+        //
+        //            // auto level2 = std::get<0>(waveformSamples.get(sample + skip));
+        //            auto colour2 = std::get<1>(waveformSamples.get(sample + skip));
+        //
+        //            auto y1 = jmap(min, -1.0f, 1.0f, (float)this->getHeight(), (float)0);
+        //            auto y2 = jmap(max, -1.0f, 1.0f, (float)this->getHeight(), (float)0);
+        //
+        //            Path p;
+        //            p.startNewSubPath(x, y1);
+        //            p.lineTo(x, y2);
+        //            p.closeSubPath();
+
+        // TODO: colour gradient
+        //            //             Create a gradient from colour1 to colour2
+        //            ColourGradient gradient(colour1, x, y1,
+        //                                    colour2, x+1, y2, false);
+        ////            g.setGradientFill(gradient);
+        ////            g.fillPath(p);
+        //            GradientPathStrokeType gradientStroke(1.0f, gradient);
+        //            g.strokePath(p, gradientStroke);
+
+        //            g.setColour(colour1);
+        //            g.strokePath(p, PathStrokeType(1.0f));
+
+        //            x++;
+        //        }
     }
 
     void RGBMeter::timerCallback()
--- Source/RGBMeter.h
+++ Source/RGBMeter.h
@@ -28,21 +28,6 @@ namespace juce
             full = head == tail;
         }
 
-        // T* getWritePointer()
-        // {
-        //     return &buffer[head];
-        // }
-
-        // void incrementWritePointer()
-        // {
-        //     head = (head + 1) % buffer.size();
-        //     if (full)
-        //     {
-        //         tail = (tail + 1) % buffer.size();
-        //     }
-        //     full = head == tail;
-        // }
-
         T get(size_t index) const
         {
             if (index >= size())
@@ -91,34 +76,34 @@ namespace juce
         bool full;
     };
 
-    class GradientPathStrokeType : public PathStrokeType
-    {
-    public:
-        GradientPathStrokeType(float thickness, ColourGradient gradient)
-            : PathStrokeType(thickness), gradient(gradient)
-        {
-        }
-
-        void createStrokedPath(Path& destPath, const Path& sourcePath, const AffineTransform& transform = AffineTransform()) const
-        {
-            PathStrokeType::createStrokedPath(destPath, sourcePath, transform);
-
-            Path::Iterator it(destPath);
-            while (it.next())
-            {
-                if (it.elementType == Path::Iterator::lineTo || it.elementType == Path::Iterator::cubicTo || it.elementType == Path::Iterator::quadraticTo)
-                {
-                    float position = Point<float>(it.x1, it.y1).getDistanceFrom(sourcePath.getPointAlongPath(0.0f)) / sourcePath.getLength();
-                    Colour colour = gradient.getColourAtPosition(position);
-                    destPath.addLineSegment(Line<float>(it.x1, it.y1, it.x2, it.y2), getStrokeThickness());
-                    destPath.applyTransform(AffineTransform::translation(it.x1, it.y1));
-                }
-            }
-        }
-
-    private:
-        ColourGradient gradient;
-    };
+//    class GradientPathStrokeType : public PathStrokeType
+//    {
+//    public:
+//        GradientPathStrokeType(float thickness, ColourGradient gradient)
+//            : PathStrokeType(thickness), gradient(gradient)
+//        {
+//        }
+//
+//        void createStrokedPath(Path& destPath, const Path& sourcePath, const AffineTransform& transform = AffineTransform()) const
+//        {
+//            PathStrokeType::createStrokedPath(destPath, sourcePath, transform);
+//
+//            Path::Iterator it(destPath);
+//            while (it.next())
+//            {
+//                if (it.elementType == Path::Iterator::lineTo || it.elementType == Path::Iterator::cubicTo || it.elementType == Path::Iterator::quadraticTo)
+//                {
+//                    float position = Point<float>(it.x1, it.y1).getDistanceFrom(sourcePath.getPointAlongPath(0.0f)) / sourcePath.getLength();
+//                    Colour colour = gradient.getColourAtPosition(position);
+//                    destPath.addLineSegment(Line<float>(it.x1, it.y1, it.x2, it.y2), getStrokeThickness());
+//                    destPath.applyTransform(AffineTransform::translation(it.x1, it.y1));
+//                }
+//            }
+//        }
+//
+//    private:
+//        ColourGradient gradient;
+//    };
 
     class RGBMeter : public Component, Timer
     {
@@ -126,23 +111,25 @@ namespace juce
     public:
         RGBMeter();
 
-//        void prepare(dsp::ProcessSpec spec);
         void pushSamples(const AudioBuffer<float> &buffer);
-        //        void pushTuple(float level, Colour colour);
         void paint(Graphics &g) override;
-        
+        void timerCallback() override;
 
     private:
 //        AudioDeviceManager device;
-        int slidingWindowSize = 2048;
+//        int slidingWindowSize = 2048;
         int displayLength = 3;  // in seconds
-        int sampleRate = 44100; // temporary
-        double skip;
+        int sampleRate = 44100; // temp hardcode
+        int bufferLength = displayLength * sampleRate;
+        double chunkSize;
+        double counter;
+        int width = 0;
         int offset = 0;
-        AudioBuffer<float> slidingWindow;
-        // Array<std::tuple<float, Colour>> waveformSamples;
-        CircularBuffer<std::tuple<float, Colour>> waveformSamples{displayLength * sampleRate};
-        void timerCallback() override;
+        AudioBuffer<float> chunkBuffer;
+//        AudioBuffer<float> slidingWindow;
+        CircularBuffer<std::tuple<float, Colour>> waveformSamples{bufferLength};
+        
+        CircularBuffer<std::tuple<Range<float>, Colour>> displayBuffer{0};
     };
     ;
 
